6076f32254b9ddd6b5f71af9a37449342fff2502
Juan8bits <juan160820@gmail.com>
2020-12-09 02:25:21 +0000

    Pep8 style correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num % 10 > num / 10:#Units > tens
+    if num % 10 > num / 10:  # Units > tens
         if num < 89 and num > 0:
             print('{:02d}, '.format(num), end='')
         elif num == 89:
