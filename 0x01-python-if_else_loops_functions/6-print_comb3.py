dd123757462ef38adfb2828ca96820b26cca771e
Juan8bits <juan160820@gmail.com>
2020-12-09 02:24:06 +0000

    Recode task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for dec in range(0, 10):
-    for uni in range(0, 10):
-        if uni != dec and uni > dec:
-            if uni > 1:
-                print(', ', end='')
-            print('{:d}{:d}'.format(dec, uni), end='')
-print('\n')
+for num in range(0, 100):
+    if num % 10 > num / 10:#Units > tens
+        if num < 89 and num > 0:
+            print('{:02d}, '.format(num), end='')
+        elif num == 89:
+            print('{:02d}'.format(num))
