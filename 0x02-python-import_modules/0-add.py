7a69f5d07c187803ea983cc5f3eb29fb96b299f1
Juan8bits <juan160820@gmail.com>
2020-12-10 23:28:50 +0000

    Change import way

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add
+from add_0 import add
 a = 1
 b = 2
 if __name__ == "__main__":
-    print('{} + {} = {}'.format(a, b, add.add(a, b)))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
