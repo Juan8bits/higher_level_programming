967e26a7dd4b05368e60796bda63508a52346184
Juan8bits <juan160820@gmail.com>
2020-12-10 02:55:29 +0000

    Create task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+* insert_node - Function that inserts node in specifically position
+* in a linked list.
+* @head: Double pointer with address to head node.
+* @number: Position to set new node.
+* Return: Address to new node or Null if it failed.
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *copyhead;
+	listint_t *prenode;
+
+	copyhead = *head;
+	prenode = *head;
+/* Creation of new node */
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	if (copyhead == NULL)
+		copyhead = new;
+	else
+	{
+/* Search position to new node */
+		while (copyhead)
+		{
+			if (copyhead->n > new->n)
+			{
+				new->next = copyhead;
+				prenode->next = new;
+				break;
+			}
+			else if (copyhead->next == NULL)
+			{
+				copyhead->next = new;
+				break;
+			}
+			prenode = copyhead;
+			copyhead = copyhead->next;
+		}
+	}
+	return (new);
+}
