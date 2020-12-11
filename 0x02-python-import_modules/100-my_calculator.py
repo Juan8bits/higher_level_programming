5f5f72f7ed624e6f31a1fa71579d782a216d28fa
Juan8bits <juan160820@gmail.com>
2020-12-11 03:45:47 +0000

    Fix pep8 style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-    
         if '+' == sys.argv[2]:
             print('{} + {} = {}'.format(a, b, add(a, b)))
         elif '-' == sys.argv[2]:
