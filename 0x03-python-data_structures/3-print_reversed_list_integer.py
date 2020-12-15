2e10b205509b362f5099affe6a9838e40a248099
Juan8bits <juan160820@gmail.com>
2020-12-15 22:13:40 +0000

    Remove module

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for idx in my_list:
+    for idx in reversed(my_list):
         print('{:d}'.format(idx))
