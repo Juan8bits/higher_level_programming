ff491a570257cef7904ca2e927a7da7fedcb4984
Juan8bits <juan160820@gmail.com>
2020-12-15 20:21:47 +0000

    Fix pep8 style

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]): 
-    my_list.reverse();    
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
     for idx in my_list:
-        print('{}'.format(idx))
+        print('{:d}'.format(idx))
