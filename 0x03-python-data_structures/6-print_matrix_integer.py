872a28bb0ddf8b6b08073b91f097c6deb8852e82
Juan8bits <juan160820@gmail.com>
2020-12-16 03:45:36 +0000

    pep8 style correction and fix maximun len in 7-add_tuple.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in range(len(matrix)):
-            separador = ''
-            for j in range(len(matrix[i])):
-                print('{}{}'.format(separador, matrix[i][j]), end='')
-                separador = ' '
-            print()
+    for i in matrix:
+        separator = ''
+        for j in i:
+            print('{}{:d}'.format(separator, j), end='')
+            separator = ' '
+        print('')
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     leng = len(sentence)
     if leng > 0:
         return (leng, sentence[0])
-    return (0,None)
+    return (0, None)
