f046d8d908cc2ee230c290bccaa4d93f8630f940
Juan8bits <juan160820@gmail.com>
2021-01-05 03:41:28 +0000

    Pep8 style correction

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1,3):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
@@ -11,4 +11,3 @@ def magic_calculation(a, b):
             result = a + b
             break
     return result
-            
