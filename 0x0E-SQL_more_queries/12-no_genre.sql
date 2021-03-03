b9e2ccd66d4994160cee45f3159b0090d19f6d2d
Juan8bits <juan160820@gmail.com>
2021-03-03 00:15:40 +0000

    Create 11 & 12 file task

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+--
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_show_genres.show_id = NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
