3bb5728dbe6c20dd1465da8980ecf4d5f1ffd2c1
Juan8bits <juan160820@gmail.com>
2021-03-03 03:33:15 +0000

    Change to LEFT JOIN to add NULL genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,6 +6,6 @@
 -- The database name will be passed as an argument of the mysql command.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
