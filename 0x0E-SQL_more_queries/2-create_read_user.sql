06e6117b98bc91c1d1cf60e85f00ccc58504a4b8
Juan8bits <juan160820@gmail.com>
2021-03-03 03:36:57 +0000

    Try to fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
 -- Script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON user_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
