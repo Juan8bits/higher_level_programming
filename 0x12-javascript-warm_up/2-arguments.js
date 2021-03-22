8923ad4be1765423ddb07fbb83e6a59937541c5a
Juan8bits <juan160820@gmail.com>
2021-03-22 23:54:16 +0000

    Fit identation level for task 3 and add "Arguments" state

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 // Script that prints a message depending of the number of arguments passed.
 if (process.argv[2]) {
-  console.log('Argument found');
+  if (process.argv[3]) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
 } else {
   console.log('No argument');
 }
