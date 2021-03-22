620af0af9df6a385b92b0f604ee715fc4fb3c286
Juan8bits <juan160820@gmail.com>
2021-03-22 23:45:17 +0000

    Fix semistandard style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
    if the first argument can be converted to an integer.
 */
 if (process.argv[2] && !isNaN(Number(process.argv[2]))) { 
-    console.log('My number: ' + parseInt(Number(process.argv[2])));
+  console.log('My number: ' + parseInt(Number(process.argv[2])));
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
