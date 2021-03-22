93a2378e197b65217c0bfa0adc94701228c0b567
Juan8bits <juan160820@gmail.com>
2021-03-22 23:47:15 +0000

    Remove Trailingi whitespace

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 /* Script that prints My number: <first argument converted in integer>
    if the first argument can be converted to an integer.
 */
-if (process.argv[2] && !isNaN(Number(process.argv[2]))) { 
+if (process.argv[2] && !isNaN(Number(process.argv[2]))) {
   console.log('My number: ' + parseInt(Number(process.argv[2])));
 } else {
   console.log('Not a number');
