9d3ebf37f721916f5342b90de7ac680feb34cf95
Juan8bits <juan160820@gmail.com>
2021-03-23 02:38:10 +0000

    Create 16 & 17 file task

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Adding a new function incr that increments the integer value.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  myObject.value += 1;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
