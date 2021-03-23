d0a9ea5bab3c80ec79c3e39db01da6ff1de6c94c
Juan8bits <juan160820@gmail.com>
2021-03-23 00:53:54 +0000

    Change maximun length of an array check

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Script that searches the second biggest integer in the list of arguments.
 const arr = process.argv.slice(2);
-if (arr.length <= 2) {
+if (arr.length <= 1) {
   console.log('0');
 } else {
   arr.sort(function (a, b) { return b - a; });
-  console.log(arr[1]);
+  console.log(parseInt(arr[1]));
 }
