ec50d5a0c1b52443bc5fbe12017521373e2c1fcb
Juan8bits <juan160820@gmail.com>
2021-03-23 01:33:03 +0000

    Change by regular factorial expression

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // Script that computes and prints a factorial.
-function factorial (Value, Step) {
-  if (Step === 1) return Value;
-  Value = factorial(Value * (Step - 1), Step - 1);
-  return Value;
+function factorial (Step) {
+  if (Step === 1) return Step;
+  Step = Step * factorial(Step - 1);
+  return Step;
 }
 
 let num = parseInt(process.argv[2]);
 if (isNaN(num) || num === 0) {
   num = 1;
 }
-console.log(factorial(num, num));
+console.log(factorial(num));
