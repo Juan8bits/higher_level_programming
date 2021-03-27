2ead334702454f1f0e736ad3efd684d4daf02d0b
Juan8bits <juan160820@gmail.com>
2021-03-27 14:29:20 +0000

    Add upper/lower case sensitivity, again

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=user, passwd=passwd, db=d_b)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY \
-                    id ASC;", (argv[4],))
+    cursor.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY \
+                    id ASC;".format(argv[4]))
     for row in cursor.fetchall():
         print(row)
     cursor.close()
