5b18804d17bd387a4e67520896b2dc72bf59a1d5
Juan8bits <juan160820@gmail.com>
2021-03-27 21:35:58 +0000

    Fix importation issue, revert session.close() method

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,15 +6,16 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == 'main':
+if __name__ == '__main__':
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     obj = session.query(State).filter(State.id == 2).first()
     obj.name = "New Mexico"
     session.commit()
+    session.close()
