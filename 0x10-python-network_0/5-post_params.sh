37beb042ed735ff7bec9697903ddd8e0788b1351
Juan8bits <juan160820@gmail.com>
2021-04-09 00:44:10 +0000

    Add silence mode

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that do a POST request and sent two values/variables.
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -sX POST "$1"
