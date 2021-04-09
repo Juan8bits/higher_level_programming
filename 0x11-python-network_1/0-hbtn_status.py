285b57701d684e001dd0d19a1c70a2e36f8768f4
Juan8bits <juan160820@gmail.com>
2021-04-09 21:21:27 +0000

    Add guards

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,11 @@
 """
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    data = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(data)))
-    print('\t- content: {}'.format(data))
-    print('\t- utf8 content: {}'.format(data.decode()))
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        data = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode()))
