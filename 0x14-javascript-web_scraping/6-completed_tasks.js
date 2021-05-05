e82766383d1bf248feff6e54ed0636d6afdd27c9
Juan8bits <juan160820@gmail.com>
2021-05-05 03:59:19 +0000

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const myObj = {};
+
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const jsonResponse = JSON.parse(body);
+    for (let i = 0; jsonResponse[i]; i++) {
+      if (jsonResponse[i].completed === true) {
+        if (jsonResponse[i].userId in myObj) {
+          myObj[jsonResponse[i].userId] += 1;
+        } else {
+          myObj[jsonResponse[i].userId] = 1;
+        }
+      }
+    }
+  }
+  console.log(myObj);
+});
