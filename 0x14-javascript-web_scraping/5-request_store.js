e7b5719715928104c76110f730f4b3acbbcb8082
Juan8bits <juan160820@gmail.com>
2021-05-05 03:40:11 +0000

    Add task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (error, response) {
+  if (error) {
+    return console.error(error);
+  }
+  fs.writeFile(file, response.body, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  }
+  );
+}
+);
