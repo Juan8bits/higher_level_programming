2647b5cd5879de8c97074b56014d9b942c119492
Juan8bits <juan160820@gmail.com>
2021-05-05 02:38:08 +0000

    add task 0 and 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const filePath = process.argv[2];
+const fs = require('fs');
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+}
+);
