a4dc57bb03cb96b4222e3cdc6a00b5b27c53d2b3
juan8bits <juan160820gmail.com>
2021-05-12 18:50:51 -0500

    Fix semistandard

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -2,7 +2,7 @@ $(function () {
   const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
   $.get(url, function (data, status) {
     if (status === 'success') {
-      $('DIV#hello').text(`${data.hello}`)
+      $('DIV#hello').text(`${data.hello}`);
     }
   });
 });
